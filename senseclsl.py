d9cefded52b1f5067124213f70ff62bb01641736
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2021-06-16 15:11:26 +0200

    new script provided by Etienne

---
+++ senseclsl.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import spot
+import sys
+
+# Usage script.py file.hoa
+
+aut = spot.automaton(sys.argv[1])
+neg = spot.dualize(aut)
+neg = neg.postprocess('TGBA')
+st_inv = spot.is_stutter_invariant(aut)
+cl_inv = spot.product(spot.closure(aut), neg).is_empty()
+sl_inv = spot.product(spot.sl(aut), neg).is_empty()
+
+print('#is_stutter,is_sl_ins,is_cl_ins')
+print(st_inv+0, sl_inv+0, cl_inv+0)
