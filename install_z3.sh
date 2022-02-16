5fb5a9fb6804b7c80aa325cad111d7bde9176a9c
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2022-02-16 17:33:20 +0100

    now grabbing directly from Z3 distribution
    historically this was more painful, but they are now GH aligned.

--- install_z3.sh
+++ install_z3.sh
@@ -3,9 +3,21 @@
 set -x
 set -e
 
-if [ ! -f z3/bin/z3 ] ; then 
-	wget --progress=dot:mega https://yanntm.github.io/ITS-Tools-Dependencies/z3.tgz
-	tar xzf z3.tgz
-	rm z3.tgz
+
+if [ ! -f z3.zip ] ; then
+	# using z3 4.8.14, available on GitHub   
+	export DLURL=https://github.com/Z3Prover/z3/releases/download/z3-4.8.14/z3-4.8.14-x64-glibc-2.31.zip	
+	wget --progress=dot:mega $DLURL
+	mv z3-4.8.14-x64-glibc-2.31.zip	z3.zip
 fi
+
+if [ ! -d z3 ] ; then 
+    unzip z3.zip
+    mv z3*/ z3dl/ 
+    mkdir z3
+    mkdir z3/bin
+    cp z3dl/bin/z3* z3/bin/
+    rm -rf z3dl/
+fi
+
 
