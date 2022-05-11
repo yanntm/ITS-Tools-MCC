c6371725048fdac1eb96ba15b008ea870fc26e30
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2022-05-11 14:18:35 +0200

    ITS-commandline repo has been merged

--- install_eclipse.sh
+++ install_eclipse.sh
@@ -5,7 +5,7 @@ set -x
 if [ ! -f itstools/its-tools ] ; then 
 	mkdir itstools
 	cd itstools
-	wget --progress=dot:mega https://yanntm.github.io/ITS-commandline/fr.lip6.move.gal.itscl.product-linux.gtk.x86_64.zip
+	wget --progress=dot:mega https://lip6.github.io/ITSTools/fr.lip6.move.gal.itscl.product-linux.gtk.x86_64.zip
 	unzip fr.lip6.move.gal.itscl.product-linux.gtk.x86_64.zip
 	rm fr.lip6.move.gal.itscl.product-linux.gtk.x86_64.zip
 	cd ..
