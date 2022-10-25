5b4336d166eb630f79565276160e68b0ac75f08f
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2022-10-25 12:29:09 +0200

    compact to match script of the SOCP paper

--- senseclsl.py
+++ senseclsl.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-import spot
-import sys
+import spot, sys
 
 # Usage script.py file.hoa
 
 aut = spot.automaton(sys.argv[1])
-neg = spot.complement(aut)
-neg = neg.postprocess('TGBA')
-st_inv = spot.is_stutter_invariant(aut)
-cl_inv = spot.product(spot.closure(aut), neg).is_empty()
-sl_inv = spot.product(spot.sl(aut), neg).is_empty()
+neg = spot.complement(aut).postprocess('TGBA')
+short_inv = spot.product(spot.closure(aut), neg).is_empty()
+len_inv = spot.product(spot.sl(aut), neg).is_empty()
 
 print('#is_stutter,is_lengthening_ins,is_shortening_ins')
-print(st_inv+0, sl_inv+0, cl_inv+0)
+print((short_inv and len_inv)+0, len_inv+0, short_inv+0)
