0f9cc55e0b4befdb387aeeac9af92872a99d27fd
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2023-02-22 11:59:26 +0100

    a file for install of dependencies on debian

---
+++ install_packages.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+# install as root
+apt-get update
+
+# packages for ITS-tools  
+apt-get install python3 unzip openjdk-17-jdk psmisc gcc libpopt-dev
+
+# spot python library
+wget -q -O - https://www.lrde.epita.fr/repo/debian.gpg | apt-key add -
+echo 'deb http://www.lrde.epita.fr/repo/debian/ stable/' >> /etc/apt/sources.list
+apt-get update
+apt-get install python3-spot 
+
+
